e06243f5ac428d0a5f95f1ef117cb0c0b1c83780
whosesmile <lovemoon@yeah.net>
2016-07-21 14:35:37 +0800

    add update logic to deploy script

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
 var cpus = require('os').cpus().length;
 var child_process = require('child_process');
 
+console.warn('\n更新代码:');
+child_process.execSync('git pull origin master', {
+  stdio: 'inherit',
+});
+
 console.warn('\n发布静态:');
 child_process.execSync('gulp dist', {
   stdio: 'inherit',
   cwd: './assets',
 });
 
-console.warn('\n编译ES6:');
+console.warn('\n编译代码:');
 
 child_process.execSync('npm run build', {
   stdio: 'inherit',
 });
 
+console.warn('\n停止服务:');
 try {
   child_process.execSync('pm2 delete legend', {
     stdio: 'inherit',
@@ -20,6 +26,7 @@ try {
 }
 catch (e) {}
 
+console.warn('\n重启服务:');
 child_process.execSync('pm2 start server.js --name=legend -i ' + cpus, {
   stdio: 'inherit',
 });
