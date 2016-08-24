1161482b392bc5743f1fe8bafaf7979911a61482
whosesmile <lovemoon@yeah.net>
2016-08-24 17:52:55 +0800

    update mysql password

--- app/config/index.js
+++ app/config/index.js
@@ -35,7 +35,7 @@ module.exports = {
     host: '127.0.0.1',
     port: '3306',
     username: 'root',
-    password: '123456',
+    password: 'legend.life!@#',
     dbname: 'legend',
     dialect: 'mysql',
   }
